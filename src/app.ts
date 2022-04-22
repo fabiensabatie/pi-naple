82017dd040d140bf97c9026fd5d072f7db4c48a2
Fabien Sabatié <fabien.sabatie@net-must.com>
2022-04-22 07:16:02 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ import { OpenNDSController } from './adapters/controllers/opennds.controller';
 const app = express();
 const port = 8010;
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	console.log("MIDDlEWARE", req.query)
+	next();
+});
 app.get('/', OpenNDSController);
 
 app.listen(port, () => {
