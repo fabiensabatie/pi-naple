5845d13df1ab2ed4fe8cfbe2e1d125756b1c9dfd
Fabien Sabatié <fabien.sabatie@net-must.com>
2022-04-22 06:48:11 +0200

    update

--- src/adapters/controllers/opennds.controller.ts
+++ src/adapters/controllers/opennds.controller.ts
@@ -4,7 +4,7 @@ import { isAuthenticatedRequest } from "../../core/use-case/is-authenticated-req
 import { getCaptivePortalContent } from "../../core/use-case/get-captive-portal-content.use-case";
 
 export const OpenNDSController = (request: Request, response: Response) => {
-    if (!request.query.fas) return response.send("Coucpu")
+    // if (!request.query.fas) return response.send("Coucpu")
     if (isAuthenticatedRequest(request.query.redir as string, request.query.tok as string))
         return response.redirect(getRedirectUrl(request.query.redir as string, request.query.tok as string))
     return response.send(getCaptivePortalContent({ fasQuery: request.query.fas as string }));
