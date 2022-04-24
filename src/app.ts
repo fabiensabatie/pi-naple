cc8a00ad3714123d95edad481a08167556502b6e
Fabien Sabatié <fabien.sabatie@net-must.com>
2022-04-24 08:34:56 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import { OpenNDSController } from './adapters/controllers/opennds.controller';
 
 const app = express();
-const port = 80;
+const port = 2050;
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
