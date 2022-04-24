2c3f3934a5b542f37fd4ea1c7c20e810be662dea
Fabien Sabatié <fabien.sabatie@net-must.com>
2022-04-24 06:13:12 +0200

    update

---
+++ src/core/domain/opennds.ts
@@ -0,0 +1,66 @@
+import crypto from 'crypto';
+import fs  from 'fs';
+
+
+interface Props {
+    readonly fasQuery: string;
+    readonly iv: string;
+    readonly gateway?: string;
+    readonly gatewayname?: string;
+    readonly gatewayurl?: string;
+    readonly hid?: string;
+}
+
+const ENC_KEY = "bf3c199c2470cb477d907b1e0917c17b";
+const AUTH_CHANNEL_DIR = "/run/";
+
+
+export class OpenNds {
+
+    private decryptedFas:string = "";
+    private constructor(private props: Props) { }
+
+    get gateway() { return this.props.gateway; }
+    get gatewayname() { return this.props.gatewayname; }
+    get gatewayurl() { return this.props.gatewayurl; }
+    get hid() { return this.props.hid; }
+    get hidHash() { return crypto.createHash('sha256').update(this.props.hid + ENC_KEY).digest('hex') }
+    get gatewaynameHash() { return crypto.createHash('sha256').update(decodeURIComponent(this.props.gatewayname)).digest('hex'); }
+    get form() { return `<form action="http://${this.props.gateway}/opennds_auth/" method="get"> <input type="hidden" name="tok" value="${this.hidHash}"> <input type="hidden" name="redir" value="https://google.fr"><br> <input type="submit" value="Continue" ></form>`; }
+
+    static create(props: Props) {
+        return new OpenNds(props).decryptFas(props.fasQuery, props.iv);
+    }
+
+    public update(props: Partial<Props>) {
+        this.props = { ...this.props, ...props };
+    }
+
+    public decryptFas(encrypted: string, iv: string) {
+        const fasData = Buffer.from(encrypted, 'base64').toString('utf8');
+        const decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, iv);
+        const decrypted = decipher.update(fasData, 'base64', 'utf8');
+        this.decryptedFas = decrypted + decipher.final('utf8');
+        this.update({
+            hid : this.decryptedFas.match(/hid=\w+/g).pop().replace("hid=", ""),
+            gateway : this.decryptedFas.match(/gatewayaddress=([\w\.:])+/g).pop().replace("gatewayaddress=", ""),
+            gatewayname : this.decryptedFas.match(/gatewayname=([\w\.%])+/g).pop().replace("gatewayname=", ""),
+            gatewayurl : this.decryptedFas.match(/gatewayurl=([\w\.:%])+/g).pop().replace("gatewayurl=", ""),
+        });
+        return this;
+    }
+
+    public async upsertAuthCommunicationChannelDir() {
+        const dir = `${AUTH_CHANNEL_DIR}${this.gatewaynameHash}`;
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+        await this.upsertAuthCommunicationChannelFile(`${dir}/${this.hidHash}`, "");
+    }
+
+    public async upsertAuthCommunicationChannelFile(filePath: string, data: string) {
+        try {
+          await fs.promises.readFile(filePath)
+        } catch (error) {
+          await fs.promises.writeFile(filePath, data)
+        }
+    }
+}
--- src/core/use-case/get-captive-portal-content.use-case.ts
+++ src/core/use-case/get-captive-portal-content.use-case.ts
@@ -1,34 +1,9 @@
-import crypto from 'crypto';
+import { OpenNds } from "../domain/opennds";
 
-interface Dependencies {}
 interface Props { fasQuery : string, iv: string };
 
-
-const ENC_KEY = "bf3c199c2470cb477d907b1e0917c17b"; // set random encryption key
-// ENC_KEY and IV can be generated as crypto.randomBytes(32).toString('hex');
-
-
-
-
-export const getCaptivePortalContent =  ({ fasQuery, iv }: Props) => {
-    const fasData = Buffer.from(fasQuery, 'base64').toString('utf8');
-      var decrypt = ((encrypted) => {
-        let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, iv);
-        let decrypted = decipher.update(encrypted, 'base64', 'utf8');
-        return (decrypted + decipher.final('utf8'));
-      });
-    const fas = decrypt(fasData);
-    const hid = fas.match(/hid=\w+/g).pop().replace("hid=", "")
-    const gateway = fas.match(/gatewayaddress=([\w\.:])+/g).pop().replace("gatewayaddress=", "")
-    const gatewayurl = fas.match(/gatewayurl=([\w\.:%])+/g).pop().replace("gatewayurl=", "")
-    const hash = crypto.createHash('sha256').update(hid + "bf3c199c2470cb477d907b1e0917c17b").digest('hex');	
-    console.log(fas, hid, gateway, gatewayurl, hash)
-    return `
-        <form action="http://${gateway}/opennds_auth/" method="get">
-                <input type="hidden" name="tok" value="${hash}">
-                <input type="hidden" name="sessionlength" value="1">
-                <input type="hidden" name="redir" value="https://google.fr"><br>
-                <input type="submit" value="Continue" >
-        </form>
-    `
+export const getCaptivePortalContent =  async ({ fasQuery, iv }: Props) => {
+    const opennds = OpenNds.create({ fasQuery, iv });
+    await opennds.upsertAuthCommunicationChannelDir();
+    return opennds.form;
 }
